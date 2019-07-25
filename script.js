d6ab850d0bc8214a88e0e5ef1f166e6fd6f10be7
HanzlaKhalid <53270755+HanzlaKhalid@users.noreply.github.com>
2019-07-25 18:42:18 +0500

    forloop assignment

---
+++ script.js
@@ -0,0 +1,11 @@
+var city = prompt("Enter your city");
+var newNum = ["Lahore", "Multan", "Krachi", "Kashmir", "Jranwala", "Gujjrat", "Queta", "Pinndi"];
+for (var i = 0; i < 6; i++){
+    if(city === newNum[i]){
+        alert("Value found");    
+    }  
+}
+
+
+
+
